b78478a94b03f6086b82b7a692c18a57769557b1
Jordan Sitkin <jordan@nuts.com>
2014-12-06 14:50:48 -0800

    get rid of status light

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-bool lightOn = false;
 
 void OnControlChange(byte channel, byte controlNumber, byte amount) {
   uint16_t brightness;
@@ -12,23 +11,14 @@ void OnControlChange(byte channel, byte controlNumber, byte amount) {
     brightness = amount * 32;
   }
   pwm.setPWM(((uint8_t) controlNumber) - 1, 0, brightness);
-  if (lightOn) {
-    digitalWrite(13, LOW);
-    delay(100);
-  } else {
-    digitalWrite(13, HIGH);
-    delay(100);
-  }
 }
 
 void setup() {
   pwm.begin();
   pwm.setPWMFreq(1600);
-  pinMode(13, OUTPUT);
   // strangely, controlChange doesn't work without a noteOn handler
   usbMIDI.setHandleNoteOn(OnNoteOn);
   usbMIDI.setHandleControlChange(OnControlChange) ;
-  digitalWrite(13, HIGH);
 }
 
 void loop() {
