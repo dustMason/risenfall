7a9cb1fa5ad282d799a98aaf5533c3f1e4035e66
Jordan Sitkin <jordan@nuts.com>
2015-08-02 14:39:12 -0700

    servo limit switches

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -2,16 +2,30 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define CONTINOUS_SERVOMIN 100 // this is the 'minimum' pulse length count (out of 4096)
-#define CONTINOUS_SERVOMAX 700 // this is the 'maximum' pulse length count (out of 4096)
-#define MICRO_SERVOMIN 150 // this is the 'minimum' pulse length count (out of 4096)
-#define MICRO_SERVOMAX 2000 // this is the 'maximum' pulse length count (out of 4096)
+#define CONTINOUS_SERVOMIN 355 // this is the 'minimum' pulse length count (out of 4096)
+#define CONTINOUS_SERVOMAX 455 // this is the 'maximum' pulse length count (out of 4096)
+#define MICRO_SERVOMIN 500 // this is the 'minimum' pulse length count (out of 4096)
+#define MICRO_SERVOMAX 2400 // this is the 'maximum' pulse length count (out of 4096)
 #define CURVE -4.5 // log curve weight (between -10 and 10)
+
+// the continuous servo on table 3:
+// servo stops spinning when midi control is set at 84
+// 84 / 128 = 0.6562
+// 0.6562 * 700 = 459.34
+// servo stops spinning at 405 pulsewidth
+#define TABLE_3_SERVO_LIMIT_LEFT 22
+#define TABLE_3_SERVO_LIMIT_RIGHT 23
+#define TABLE_3_SERVO_OFF_PULSELENGTH 405
 
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
 Adafruit_PWMServoDriver board3 = Adafruit_PWMServoDriver(0x42);
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver(0x43);
+
+boolean table_3_servo_limit_left = false;
+boolean table_3_servo_limit_right = false;
+boolean table_3_servo_turning_left = false;
+boolean table_3_servo_turning_right = false;
 
 void OnControlChange(byte channel, byte controlNumber, byte amount) {
   uint8_t pwmChannel = ((uint8_t) controlNumber) - 1;
@@ -19,6 +33,25 @@ void OnControlChange(byte channel, byte controlNumber, byte amount) {
     uint16_t pulselen;
     if (pwmChannel < 8) {
       pulselen = map(amount, 0, 127, MICRO_SERVOMIN, MICRO_SERVOMAX);
+    } else if (pwmChannel == 15) {
+      if (
+        (table_3_servo_limit_right && amount < 84) || // trying to turn right
+        (table_3_servo_limit_left && amount > 85) // trying to turn left
+      ) {
+        pulselen = uint16_t(TABLE_3_SERVO_OFF_PULSELENGTH);
+      } else {
+        if (amount < 84) {
+          table_3_servo_turning_left = false;
+          table_3_servo_turning_right = true;
+        } else if (amount > 85) {
+          table_3_servo_turning_left = true;
+          table_3_servo_turning_right = false;
+        } else {
+          table_3_servo_turning_left = false;
+          table_3_servo_turning_right = false;
+        }
+        pulselen = map(amount, 0, 127, CONTINOUS_SERVOMIN, CONTINOUS_SERVOMAX);
+      }
     } else {
       pulselen = map(amount, 0, 127, CONTINOUS_SERVOMIN, CONTINOUS_SERVOMAX);
     }
@@ -40,6 +73,18 @@ void OnControlChange(byte channel, byte controlNumber, byte amount) {
   }
 }
 
+void checkTable3ServoLimits() {
+  table_3_servo_limit_left = (digitalRead(TABLE_3_SERVO_LIMIT_LEFT) == LOW);
+  table_3_servo_limit_right = (digitalRead(TABLE_3_SERVO_LIMIT_RIGHT) == LOW);
+  if (table_3_servo_limit_right && table_3_servo_turning_right) {
+    servos.setPWM(15, 0, uint16_t(TABLE_3_SERVO_OFF_PULSELENGTH));
+    table_3_servo_turning_right = false;
+  } else if (table_3_servo_limit_left && table_3_servo_turning_left) {
+    servos.setPWM(15, 0, uint16_t(TABLE_3_SERVO_OFF_PULSELENGTH));
+    table_3_servo_turning_left = false;
+  }
+}
+
 void setup() {
   board1.begin();
   board1.setPWMFreq(1600);
@@ -51,9 +96,14 @@ void setup() {
   servos.setPWMFreq(60); // Analog servos run at ~60 Hz updates
   usbMIDI.setHandleNoteOn(OnNoteOn);
   usbMIDI.setHandleControlChange(OnControlChange);
+  pinMode(TABLE_3_SERVO_LIMIT_LEFT, INPUT_PULLUP);
+  pinMode(TABLE_3_SERVO_LIMIT_RIGHT, INPUT_PULLUP);
 }
 
-void loop() { usbMIDI.read(); }
+void loop() {
+  checkTable3ServoLimits();
+  usbMIDI.read();
+}
 
 void OnNoteOn(byte channel, byte note, byte velocity) {
   // zilch. strangely, controlChange doesn't work without a noteOn handler
@@ -69,15 +119,15 @@ float fscale(float originalMin, float originalMax, float newBegin, float newEnd,
   boolean invFlag = 0;
   if (curve > 10) curve = 10;
   if (curve < -10) curve = -10;
-  curve = (curve * -.1); // postive numbers give more weight to high end on output 
+  curve = (curve * -.1); // postive numbers give more weight to high end on output
   curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
   if (inputValue < originalMin) { inputValue = originalMin; }
   if (inputValue > originalMax) { inputValue = originalMax; }
   originalRange = originalMax - originalMin;
-  if (newEnd > newBegin) { 
+  if (newEnd > newBegin) {
     newRange = newEnd - newBegin;
   } else {
-    newRange = newBegin - newEnd; 
+    newRange = newBegin - newEnd;
     invFlag = 1;
   }
   zeroRefCurVal = inputValue - originalMin;
@@ -85,8 +135,8 @@ float fscale(float originalMin, float originalMax, float newBegin, float newEnd,
   if (originalMin > originalMax ) { return 0; }
   if (invFlag == 0){
     rangedValue = (pow(normalizedCurVal, curve) * newRange) + newBegin;
-  } else {   
-    rangedValue = newBegin - (pow(normalizedCurVal, curve) * newRange); 
+  } else {
+    rangedValue = newBegin - (pow(normalizedCurVal, curve) * newRange);
   }
   return rangedValue;
 }
