0ae26d6cd04b12505e7249c009785d7428f64482
Austin Hill <totalhappy@gmail.com>
2023-02-25 12:09:23 -0800

    Update sketch.ino

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -2,25 +2,35 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define CONTINOUS_SERVOMIN 355 // this is the 'minimum' pulse length count (out of 4096)
-#define CONTINOUS_SERVOMAX 455 // this is the 'maximum' pulse length count (out of 4096)
-#define MICRO_SERVOMIN 500 // this is the 'minimum' pulse length count (out of 4096)
-#define MICRO_SERVOMAX 2400 // this is the 'maximum' pulse length count (out of 4096)
-#define CURVE -4.5 // log curve weight (between -10 and 10)
+#define CONTINOUS_SERVOMIN 172 // see `table 3 servo formula` comment below
+#define CONTINOUS_SERVOMAX 565 // see `table 3 servo formula` comment below
+#define MICRO_SERVOMIN 500     // this is the 'minimum' pulse length count (out of 4096)
+#define MICRO_SERVOMAX 2400    // this is the 'maximum' pulse length count (out of 4096)
+#define CURVE -4.5             // log curve weight (between -10 and 10)
 
-// the continuous servo on table 3:
-// servo stops spinning when midi control is set at 84
-// 84 / 128 = 0.6562
-// 0.6562 * 700 = 459.34
-// servo stops spinning at 405 pulsewidth
-#define TABLE_3_SERVO_LIMIT_LEFT 22
-#define TABLE_3_SERVO_LIMIT_RIGHT 23
-#define TABLE_3_SERVO_OFF_PULSELENGTH 405
+// --- table 3 servo formula --
+// - 60Hz is the update cycle for table 3 servo (PWM driver sends 60 pulses per second)
+// - each pulse has 4096 ticks (4096 ticks = 1/60th of a second)
+// - 1/60th of a second = 16,666.667 useconds
+// - min/stop/max of FS90R is 700/1500/2300 useconds per the datasheet
+// min -> 700 / 16666 * 4096 = 172.03
+// stop -> 1500 / 16666 * 4096 = 368.65 (theoretical. in practice the stop value is 400)
+// max -> 2300 / 16666 * 4096 = 565.27
+// (from testing, the servo stops at midi cc value 77. 77 equates to 400 PWM value)
+
+
+#define TABLE_3_SERVO_LIMIT_LEFT 22       // pin numbner
+#define TABLE_3_SERVO_LIMIT_RIGHT 23      // pin number
+#define TABLE_3_SERVO_OFF_PULSELENGTH 400 // determined via testing
+#define TABLE_3_SERVO_MIDI_STOP 77
 
 Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
 Adafruit_PWMServoDriver board3 = Adafruit_PWMServoDriver(0x42);
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver(0x43);
+Adafruit_PWMServoDriver board4 = Adafruit_PWMServoDriver(0x44);
+Adafruit_PWMServoDriver board5 = Adafruit_PWMServoDriver(0x45); 
+Adafruit_PWMServoDriver board6 = Adafruit_PWMServoDriver(0x46);
 
 boolean table_3_servo_limit_left = false;
 boolean table_3_servo_limit_right = false;
@@ -35,15 +45,15 @@ void OnControlChange(byte channel, byte controlNumber, byte amount) {
       pulselen = map(amount, 0, 127, MICRO_SERVOMIN, MICRO_SERVOMAX);
     } else if (pwmChannel == 15) {
       if (
-        (table_3_servo_limit_right && amount < 84) || // trying to turn right
-        (table_3_servo_limit_left && amount > 85) // trying to turn left
+        (table_3_servo_limit_right && amount < TABLE_3_SERVO_MIDI_STOP) || // trying to turn right
+        (table_3_servo_limit_left && amount > TABLE_3_SERVO_MIDI_STOP)     // trying to turn left
       ) {
         pulselen = uint16_t(TABLE_3_SERVO_OFF_PULSELENGTH);
       } else {
-        if (amount < 84) {
+        if (amount < TABLE_3_SERVO_MIDI_STOP) {
           table_3_servo_turning_left = false;
           table_3_servo_turning_right = true;
-        } else if (amount > 85) {
+        } else if (amount > TABLE_3_SERVO_MIDI_STOP) {
           table_3_servo_turning_left = true;
           table_3_servo_turning_right = false;
         } else {
@@ -69,6 +79,12 @@ void OnControlChange(byte channel, byte controlNumber, byte amount) {
       board2.setPWM(pwmChannel, 0, brightness);
     } else if (channel == 3) {
       board3.setPWM(pwmChannel, 0, brightness);
+    } else if (channel == 4) {
+      board4.setPWM(pwmChannel, 0, brightness);
+    } else if (channel == 5) {
+      board5.setPWM(pwmChannel, 0, brightness);
+    } else if (channel == 6) {
+      board6.setPWM(pwmChannel, 0, brightness);
     }
   }
 }
@@ -85,20 +101,27 @@ void checkTable3ServoLimits() {
   }
 }
 
-void setup() {
+void setup() { 
   board1.begin();
   board1.setPWMFreq(1600);
   board2.begin();
   board2.setPWMFreq(1600);
   board3.begin();
   board3.setPWMFreq(1600);
+  board4.begin();
+  board4.setPWMFreq(1600);
+  board5.begin();
+  board5.setPWMFreq(1600);
+  board6.begin();
+  board6.setPWMFreq(1600);
   servos.begin();
   servos.setPWMFreq(60); // Analog servos run at ~60 Hz updates
   usbMIDI.setHandleNoteOn(OnNoteOn);
   usbMIDI.setHandleControlChange(OnControlChange);
   pinMode(TABLE_3_SERVO_LIMIT_LEFT, INPUT_PULLUP);
   pinMode(TABLE_3_SERVO_LIMIT_RIGHT, INPUT_PULLUP);
-}
+  servos.setPWM(15, 0, uint16_t(TABLE_3_SERVO_OFF_PULSELENGTH)); // start the servo OFF
+} // add code here for boards
 
 void loop() {
   checkTable3ServoLimits();
